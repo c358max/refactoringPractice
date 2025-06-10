3cb55bce7eaf80ea051f44fcbd10817efd0bc9b2
david.nam <david.nam@pulmuone.com>
2025-06-10 09:10:34 +0900

    리팩터링 작성중

--- src/main/java/com/refactoring/ch11/step10/ChargeCalculator.java
+++ src/main/java/com/refactoring/ch11/step10/ChargeCalculator.java
@@ -1,23 +1,24 @@
 package com.refactoring.ch11.step10;
 
 public class ChargeCalculator {
-    private Customer customer;
-    private int usage;
-    private Provider provider;
-
-    public ChargeCalculator(Customer customer, int usage, Provider provider) {
-        this.customer = customer;
-        this.usage = usage;
-        this.provider = provider;
-    }
-
-    public double getBaseCharge() {
-        return customer.getBaseRate() * usage;
-    }
-
-    public double getCharge() {
-        return getBaseCharge() + provider.getConnectionCharge();
-    }
+// 리팩터링 전
+//    private Customer customer;
+//    private int usage;
+//    private Provider provider;
+//
+//    public ChargeCalculator(Customer customer, int usage, Provider provider) {
+//        this.customer = customer;
+//        this.usage = usage;
+//        this.provider = provider;
+//    }
+//
+//    public double getBaseCharge() {
+//        return customer.getBaseRate() * usage;
+//    }
+//
+//    public double getCharge() {
+//        return getBaseCharge() + provider.getConnectionCharge();
+//    }
 
     // 리팩터링 후
     public static double calculateCharge(Customer customer, int usage, Provider provider) {
