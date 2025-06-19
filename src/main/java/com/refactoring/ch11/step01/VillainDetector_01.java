9f30c27bb63871116e6c064b3743f5b02190d01b
david.nam <david.nam@pulmuone.com>
2025-06-19 21:37:07 +0900

    11.1 소스 리팩터링

--- src/main/java/com/refactoring/ch11/step01/VillainDetector_01.java
+++ src/main/java/com/refactoring/ch11/step01/VillainDetector_01.java
@@ -5,6 +5,7 @@ import java.util.List;
 /**
  * 11.1 질의 함수와 변경 함수 분리하기 (Seperate Query from Modifier)
  * 리팩터링 전 : 이름을 반환(질의) + 알람실행(변경) -> 테스트 어려움, 책임 혼합
+ * 질의함수와 명령 함수를 분리하여 코드의 명확성, 테스트 용이성, 예측 가능성 향상이 목적
  */
 public class VillainDetector_01 {
     public String alertForMiscreant(List<String> people) {
--- src/test/java/com/refactoring/ch11/step01/VillainDetectorTest.java
+++ src/test/java/com/refactoring/ch11/step01/VillainDetectorTest.java
@@ -1,25 +1,47 @@
 package com.refactoring.ch11.step01;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VillainDetectorTest {
+    private final VillainDetector detector = new VillainDetector();
+
+    @DisplayName("리스트에 조커가 있을때 리턴")
     @Test
-    void testFindMiscreant() {
-        // given
-        VillainDetector detector = new VillainDetector();
-        List<String> people = List.of("배트맨", "조커", "슈퍼맨");
-
-        // when
+    void testFindMiscreant_returnsFirstMiscreant() {
+        List<String> people = Arrays.asList("배트맨", "조커", "사루만");
         String result = detector.findMiscreant(people);
-        detector.alertForMiscreant(people);
-
-        // then
         assertEquals("조커", result);
+    }
+
+    @DisplayName("리스트에 조커는 없고 사루만만 있을때 리턴")
+    @Test
+    void testFindMiscreant_returnsSecondMiscreantIfFirstNotFound() {
+        List<String> people = Arrays.asList("배트맨", "슈퍼맨", "사루만");
+        String result = detector.findMiscreant(people);
+        assertEquals("사루만", result);
+    }
+
+    @DisplayName("빌런이 없는 경우 빈 문자열 리턴")
+    @Test
+    void testFindMiscreant_returnsEmptyIfNoMiscreant() {
+        List<String> people = Arrays.asList("배트맨", "슈퍼맨", "원더우먼");
+        String result = detector.findMiscreant(people);
+        assertEquals("", result);
+    }
+
+    @DisplayName("리스트가 비어 있는 경우 처리확인")
+    @Test
+    void testFindMiscreant_returnsEmptyIfListEmpty() {
+        List<String> people = Collections.emptyList();
+        String result = detector.findMiscreant(people);
+        assertEquals("", result);
     }
 }
